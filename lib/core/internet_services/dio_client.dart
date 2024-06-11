e6aadf21d60731984aeeeef94243ff4c35f3ca15
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-11 10:06:26 +0200

    update

--- lib/core/internet_services/dio_client.dart
+++ lib/core/internet_services/dio_client.dart
@@ -1,8 +1,29 @@
+import 'package:crudapiapp/core/internet_services/path.dart';
 import 'package:dio/dio.dart';
 
-class DioClient{
-
+/// Create a singleton class to contain all Dio methods and helper functions
+class DioClient {
   DioClient._();
 
   static final instance = DioClient._();
+
+  final Dio _dio = Dio(
+    BaseOptions(
+        baseUrl: baseUrl,
+        connectTimeout: const Duration(seconds: 60),
+        receiveTimeout: const Duration(seconds: 60),
+        responseType: ResponseType.json),
+  );
+}
+
+///The Get Method
+Future<Map<String, dynamic>> get(String path,
+    {Map<String, dynamic>? queryQarameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onReceiveProgress}) async {
+  try {} catch (e) {
+    print(e);
+  }
+  throw "";
 }
