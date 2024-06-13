87ad91779e57a6e4ee60d1ef1bc6c9070658dfba
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-13 09:54:54 +0200

    update

--- lib/features/crud/domain/repository/user_repo.dart
+++ lib/features/crud/domain/repository/user_repo.dart
@@ -1,5 +1,10 @@
+import 'package:crudapiapp/features/crud/domain/model/new_user.dart';
 import 'package:crudapiapp/features/crud/domain/model/user.dart';
 
-// abstract class UserRepository{
-//   Future<List<User>>getUserList();
-// }
+//contain all the different methods to be implemented for GET, POST, PUT, and, DELETE.
+abstract class UserRepository{
+  Future<List<User>>getUserList();
+  Future<NewUser>addNewUser(String name, String job);
+  Future<NewUser>updateUser(String id, String name, String job);
+  Future<void>deleteUser(String id);
+}
